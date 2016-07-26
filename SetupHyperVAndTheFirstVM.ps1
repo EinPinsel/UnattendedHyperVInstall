faad08fd4e25e746bbd4c562002a8e74fe4a1092
Stefan Ewers <Stefan Ewers>
2016-07-26 11:30:37 +0200

    Added HyperVVMAdress. Yay WMI. Changes

--- SetupHyperVAndTheFirstVM.ps1
+++ SetupHyperVAndTheFirstVM.ps1
@@ -56,3 +56,38 @@ $iso = "C:\Users\stefa\Downloads\unattended.iso"
 Add-VMDvdDrive -VMName $m –Path $iso
 
 Start-VM -VMName $m
+
+
+# http://windowsitpro.com/hyper-v/modify-ip-configuration-vm-hyper-v-host
+
+
+$Msvm_VirtualSystemManagementService = Get-WmiObject -Namespace root\virtualization\v2 `
+    -Class Msvm_VirtualSystemManagementService 
+
+$Msvm_ComputerSystem = Get-WmiObject -Namespace root\virtualization\v2 `
+    -Class Msvm_ComputerSystem -Filter "ElementName='$m'" 
+
+$Msvm_VirtualSystemSettingData = ($Msvm_ComputerSystem.GetRelated('Msvm_VirtualSystemSettingData', `
+    'Msvm_SettingsDefineState', $null, $null, 'SettingData', 'ManagedElement', $false, $null) | % {$_})
+
+$Msvm_SyntheticEthernetPortSettingData = $Msvm_VirtualSystemSettingData.GetRelated('Msvm_SyntheticEthernetPortSettingData')
+
+$Msvm_GuestNetworkAdapterConfiguration = ($Msvm_SyntheticEthernetPortSettingData.GetRelated( `
+    'Msvm_GuestNetworkAdapterConfiguration', 'Msvm_SettingDataComponent', `
+    $null, $null, 'PartComponent', 'GroupComponent', $false, $null) | % {$_})
+
+$Msvm_GuestNetworkAdapterConfiguration.DHCPEnabled = $false
+$Msvm_GuestNetworkAdapterConfiguration.IPAddresses = @('192.168.1.1')
+$Msvm_GuestNetworkAdapterConfiguration.Subnets = @('255.255.255.0')
+$Msvm_GuestNetworkAdapterConfiguration.DefaultGateways = @('192.168.1.1')
+$Msvm_GuestNetworkAdapterConfiguration.DNSServers = @('192.168.1.1')
+
+$Msvm_VirtualSystemManagementService.SetGuestNetworkAdapterConfiguration( `
+$Msvm_ComputerSystem.Path, $Msvm_GuestNetworkAdapterConfiguration.GetText(1))
+
+
+
+# Enable PSRemoting on the host:
+Enable-PSRemoting -SkipNetworkProfileCheck
+
+# 
